e88b7c0a7769348980326fee4d0b47cfdbdc7ddd
Logon Aluno <logonaluno@local.com>
2019-11-26 22:14:52 -0300

    Ciração da classe de conexão

---
+++ SimpleBotCore/Logic/MongoDB.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace SimpleBotCore.Logic
+{
+    public class MongoDB
+    {
+        MongoClient client = null;
+
+        public MongoDB()
+        {
+            client = new MongoClient("mongodb://localhost:27017");
+        }
+
+        public bool Inserir(string comando)
+        {
+            bool commit;
+            try
+            {
+                var db = client.GetDatabase("16NET");
+                var colecao = db.GetCollection<BsonDocument>("colecao");
+
+                var dados = new BsonDocument()
+            {
+                {"id", 1 },
+                {"comando", comando}
+            };
+                colecao.InsertOne(dados);
+                commit = true;
+            }
+            catch
+            {
+                throw;
+            }
+            return commit;
+        }
+    }
+}
